e4dd4036930658befde2c25eaa2458224a036dbf
Chick25 <huynhlekimyenn@gmail.com>
2025-05-25 16:09:49 +0700

    update login

--- backend/Controller/authController.js
+++ backend/Controller/authController.js
@@ -19,8 +19,6 @@ const register = (req, res) =>{
             if(err){
                 console.error('Lỗi khi mã hóa mật khẩu: ', err.message);
                 return res.status(500).json({message: 'Lỗi máy chủ'});
-
-
             }
 
             User.create(firstname, lastname, email, hashedPassword, (err, result)=>{
@@ -38,4 +36,43 @@ const register = (req, res) =>{
     });
 };
 
-module.exports ={register};
+const login = (req, res) =>{
+    const {email, password} = req.body;
+
+    User.findByEmail(email, (err, results)=>{
+        if(err){
+            console.err('Lỗi khi kiểm tra email:', err.message);
+            return res.status(500).json({message: 'Lỗi máy chủ'});
+        }
+        if(results.length === 0){
+            return res.status(400).json({message: 'Email hoặc mật khẩu không đúng'});
+        }
+
+        const user = results[0];
+
+        bcrypt.compare(password, user.password, (err, isMatch)=>{
+            if(err){
+                console.err('Lỗi khi kiểm tra mật khẩu: ', err.message);
+                return res.status(500).json({message: 'Lỗi máy chủ'});
+            }
+            if(!isMatch){
+                return res.status(400).json({message: 'Email hoặc mật khẩu không đúng'});
+            }
+            // res.json(201).json({message:'Đăng nhập thành công', user: {id:user.id, email:user.email}});
+
+            return res.status(201).json({
+                message: 'Đăng nhập thành công',
+                user:{
+                    id: user.id,
+                    email: user.email
+                }
+            });
+
+        });
+
+    });
+};
+
+
+module.exports ={register, login};
+
